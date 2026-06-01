046aa6f3413c95a55b63aa6884e5ed7c1b0140f1
Knuchols <89404374+Knuchols@users.noreply.github.com>
2026-06-01 04:59:23 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function openModal(title, description) {
+
+    document.getElementById("modalTitle").innerText = title;
+
+    document.getElementById("modalDescription").innerText = description;
+
+    document.getElementById("projectModal").style.display = "block";
+}
+
+function closeModal() {
+
+    document.getElementById("projectModal").style.display = "none";
+}
+
+window.onclick = function(event) {
+
+    const modal = document.getElementById("projectModal");
+
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
